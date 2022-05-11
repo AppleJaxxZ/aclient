689292d4f3713084f758232f122804dd3eacafa9
Bryan A <easyrun32@gmail.com>
2022-05-11 15:34:28 -0400

    status

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,24 +1,54 @@
-import React, { useEffect } from 'react';
-import { getSubscription, signOut } from '../../redux/user/user.action';
+import React, { useEffect, useState } from 'react';
+import { signOut } from '../../redux/user/user.action';
 import Button from '../../components/Button/Button';
 import { useDispatch } from 'react-redux';
 import styles from './Dashboard.module.scss';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const Dashboard = () => {
   let navigate = useNavigate();
   const dispatch = useDispatch();
+  const [mySubscription, setMySubscription] = useState(null);
   const customer_id = useSelector(({ user }) => user.user.customer_id);
+  const getmysub = async (customer_id) => {
+    return await axios.post(
+      `${process.env.REACT_APP_BACKEND}/api/subscription/my-subscription`,
+      { customer_id: customer_id },
+      {
+        headers: {
+          Authorization: localStorage.getItem('token'),
+        },
+      }
+    );
+  };
   useEffect(() => {
     if (localStorage.getItem('token')) {
-      dispatch(getSubscription(customer_id));
+      const fetchData = async () => {
+        try {
+          const {
+            data: { active },
+          } = await getmysub(customer_id);
+
+          setMySubscription(active);
+        } catch (err) {
+          setMySubscription(null);
+        }
+      };
+
+      fetchData();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className={styles.container}>
-      We currently Only have one subscription!
+      {mySubscription ? (
+        <h1>Subscription : {mySubscription}</h1>
+      ) : (
+        <h1>You currently do not have a subscription</h1>
+      )}
       <Button
         onClick={() => {
           dispatch(signOut());
